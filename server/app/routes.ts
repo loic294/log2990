ec3f71752c24adc42b96eaadf6f9c4e7a98a42ea
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-01-23 16:48:06 -0500

    New server architecture

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from "inversify";
 import { Router, Request, Response, NextFunction } from "express";
+import glob from "glob"
 
 import Types from "./types";
 import { Index } from "./routes/index";
@@ -7,10 +8,16 @@ import { Index } from "./routes/index";
 @injectable()
 export class Routes {
 
-    public constructor(@inject(Types.Index) private index: Index) {}
+    public constructor(
+        @inject(Types.Index) private index: Index
+    ) {}
 
     public get routes(): Router {
         const router: Router = Router();
+
+        glob(`${__dirname}/modules/*`, { ignore: '**/index.js' }, (err, matches) => {
+
+        })
 
         router.get("/",
                    (req: Request, res: Response, next: NextFunction) => this.index.helloWorld(req, res, next));
---
+++ server/app/routes/user/router.ts
@@ -0,0 +1,14 @@
+
+
+export default {
+	prefix: '/user',
+	routes: [
+		{
+			method: 'GET',
+			path: '/',
+			middleware: [
+				
+			]
+		}
+	]
+}
