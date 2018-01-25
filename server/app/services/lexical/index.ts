bbfb4c346f6ccca4c84f0df2185dd639323a09fe
Camille Bm <camille.bergeron-miron@polymtl.ca>
2018-01-25 14:52:21 -0500

    Ajout fonction de base pour avoir les definitions d'un mot

--- server/app/services/lexical/index.ts
+++ server/app/services/lexical/index.ts
@@ -1,7 +1,8 @@
-
 /*
 	This service will be responsible of requesting and creating lexical content.
 */
+import axios from 'axios';
+const apiKey = "api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"; 
 
 export default class LexicalService {
 
@@ -22,4 +23,13 @@ export default class LexicalService {
 
 	}
 
+	async baseDefinition(word: string) {  
+        const url = `http://api.wordnik.com:80/v4/word.json/${word}/definitions?limit=200&${apiKey}`; 
+		try {
+			const response = await axios.get(url);
+			return response.data;
+		} catch(err){
+			throw err;
+		}
+    } 
 }
