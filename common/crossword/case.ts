98d1ed0bd8bac719743caaa83e76d77458a6e95a
Gabriel <gabcj29@hotmail.com>
2018-01-26 12:39:49 -0500

    rebase

--- common/crossword/case.ts
+++ common/crossword/case.ts
@@ -3,9 +3,13 @@ export class Case {
 
 	private _isSelected: boolean = false
 
-	constructor(private _char: String = "-") {
+	constructor(private _char: String = "-", private _x = 0, private _y = 0) {
 
 	}
+	
+	public setChar(c: string) {
+            this._char = c;
+        }
 
 	public getChar() {
 		return this._char;
@@ -22,5 +26,21 @@ export class Case {
 	public selected() {
 		return this._isSelected;
 	}
+	
+	public getX() {
+            return this._x;
+        }
+        
+        public getY() {
+            return this._y;
+        }
+        
+        public setX(x: number) {
+            this._x = x;
+        }
+        
+        public setY(y: number) {
+            this._y = y;
+        }
 
 }
