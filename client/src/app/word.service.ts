3498fd06dd0f72083fff5760d041cd9621bd2a54
nickbyd17 <nbayard17@gmail.com>
2018-01-28 13:03:54 -0500

    Correction du code en fonction des normes

--- client/src/app/word.service.ts
+++ client/src/app/word.service.ts
@@ -1,23 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable'
-import { Subject } from 'rxjs/Subject'
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
 import Word from "../../../common/lexical/word";
 
 @Injectable()
-export class WordService
-{
+export class WordService {
   private _word: Observable<Word>;
-  private _wordSubject = new Subject<Word>();
+  private _wordSubject: Subject<Word> = new Subject<Word>();
 
-  getWord(): Observable<Word> {
+  public getWord(): Observable<Word> {
     return this._word;
   }
 
-  selectWord(word: Word): void {
+  public selectWord(word: Word): void {
     this._wordSubject.next(word);
   }
 
-  constructor() {
+  public constructor() {
     this._word = this._wordSubject.asObservable();
    }
 
