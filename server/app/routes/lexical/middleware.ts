4ee0a7fa72004beaee9d150aa8b94e7e18974717
Camille Bm <camille.bergeron-miron@polymtl.ca>
2018-01-30 12:41:04 -0500

    Ajout de wordDefinition dans middleware

--- server/app/routes/lexical/middleware.ts
+++ server/app/routes/lexical/middleware.ts
@@ -25,3 +25,15 @@ export async function wordSearch(req: Request, res: Response, next: NextFunction
 
 	res.json({ lexicalResult: testResult })
 }
+
+export async function wordDefintion(req: Request, res: Response, next: NextFunction) {
+	const { level } = req.params
+	const { word } = req.params
+
+	const lexicalService = new LexicalService()
+
+	const testResult = await lexicalService.wordDefinition(level, word);
+	
+	res.json({ lexicalResult: testResult })
+
+}
