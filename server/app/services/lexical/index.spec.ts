bc07bc1db19271acac72dd9e37fcb8aa4ae55e9f
Julien Bergeron <bergeron_julien@outlook.com>
2018-02-04 20:46:15 -0500

    making linter happy on lexicalService's file

--- server/app/services/lexical/index.spec.ts
+++ server/app/services/lexical/index.spec.ts
@@ -1,19 +1,19 @@
-import { default as LexicalService } from "./index"; 
-import { assert } from 'chai'; 
+ /* tslint:disable */ 
+import { default as LexicalService } from "./index";
+import { assert } from "chai";
 
-const service = new LexicalService; 
- 
- 
-describe("wordSearch('4', 'true')", () => {
+const service: LexicalService = new LexicalService;
+
+describe("wordSearch('4', 'common')", () => {
     it("should return a 4 letter common word", () => {
-        
-        return service.wordSearch("4", "coomon").then((data: any)=> {
+
+        return service.wordSearch("4", "common").then((data: any)=> {
             assert.equal(4, data.length);
         });
     });
 });
 
-describe("wordSearch('4', 'false')", () => {
+describe("wordSearch('4', 'uncommon')", () => {
     it("should return a 4 letter common word", () => {
         
         return service.wordSearch("4", "common").then((data: any)=> {
@@ -22,31 +22,32 @@ describe("wordSearch('4', 'false')", () => {
     });
 });
 
-describe("wordSearch('2a3', 'true')", () => {
+describe("wordSearch('2a3', 'common')", () => {
     it("should return a 6 letter common word with an a at the 3 position", () => {
 
         return service.wordSearch("2a3", "uncommon").then((data: any)=> {
-            
+
             assert.equal(6, data.length);
             assert.equal("a", data[2]);
         });
     });
 });
 
-describe("wordDefinition('easy','train')", () => {   
-    it("should return the first provided definition of the word", () => {   
-        return service.wordDefinition("easy","train").then((definition: any)=>{  
+
+describe("wordDefinition('easy','train')", () => {
+    it("should return the first provided definition of the word", () => {
+        return service.wordDefinition("easy","train").then((definition: any)=>{
             assert.notInclude(definition, "train", "Should not include the word train in the definition");
             assert.equal(definition, "A series of connected railroad cars pulled or pushed by one or more locomotives.");
-        });   
-     });   
-}); 
+        });
+     });
+});
 
-describe("wordDefinition('hard','train')", () => {   
-    it("should return an alternative definition of the word", () => {   
-        return service.wordDefinition("hard","train").then((definition: any)=>{  
+describe("wordDefinition('hard','train')", () => {
+    it("should return an alternative definition of the word", () => {
+        return service.wordDefinition("hard","train").then((definition: any) => {
             assert.notInclude(definition, "train", "Should not include the word train in the definition");
             assert.notEqual(definition, "A series of connected railroad cars pulled or pushed by one or more locomotives.");
-        });    
-     });   
-}); 
+        });
+     });
+});
