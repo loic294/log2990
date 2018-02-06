11bef14ec34bedb1de553ef7e1378b314a721672
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-02-05 23:27:36 -0500

    Making tslint happy part 3

--- server/app/routes/index.ts
+++ server/app/routes/index.ts
@@ -1,24 +1,28 @@
 import * as fs from "fs";
 import { Request, Response, NextFunction } from "express";
 
-const asyncMiddleware = (fn: any) =>
+const asyncMiddleware: Function = (fn: Function) =>
   (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next))
       .catch(next);
   };
 
-export default function (app: any) {
+export default (app: any) => {
 
-	fs.readdirSync(__dirname).forEach(function(filename) {
+    fs.readdirSync(__dirname).forEach((filename: string) => {
 
     if (!filename.includes(".js")) {
-			const { routes, base } = require(`${__dirname}/${filename}/routes`).default;
-			routes.forEach(({ method, path, middleware }: { method: string, path: string, middleware: Array<(req: Request, res: Response, next: NextFunction ) => void> }) => {
-				app[method.toLowerCase()](`${base}${path}`, ...middleware.map((m) => asyncMiddleware(m)));
-			});
-		}
+            const { routes, base } = require(`${__dirname}/${filename}/routes`).default;
+            routes.forEach(
+                ({ method, path, middleware }: {
+                    method: string,
+                    path: string,
+                    middleware: Array<(req: Request, res: Response, next: NextFunction ) => void> }) => {
+                app[method.toLowerCase()](`${base}${path}`, ...middleware.map((m) => asyncMiddleware(m)));
+            });
+        }
 
   });
 
-	return app;
-}
+    return app;
+};
--- server/app/routes/user/middleware.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export async function me(req: Request, res: Response, next: NextFunction) {
-	res.json({ succes: true });
-}
