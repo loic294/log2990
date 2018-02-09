ce7fba519ed90282d481aeed1445fd5311ac0241
Julien Bergeron <bergeron_julien@outlook.com>
2018-02-09 10:32:34 -0500

    Another merge problem fixed

--- common/lexical/word.ts
+++ common/lexical/word.ts
@@ -11,8 +11,13 @@ export default class Word {
         private _position : Array<number>,
         private _orientation : Orientation,
         private _index : number,
-        private _isValidated: boolean = false
-    ) {}
+		private _isValidated: boolean = false,
+		private _length: number = 0
+    ) {
+        if (!_length) {
+            this._length = this._name.length
+        }
+    }
 
     public get name() {
         return this._name;
@@ -49,10 +54,6 @@ export default class Word {
     public get direction() {
         return this._orientation;
     }
-
-    public get length() {
-        return this._name.length;
-    }
     
     public get validated() {
         return this._isValidated
@@ -68,6 +69,14 @@ export default class Word {
 
     public set index( index : number ) {
         this._index = index;
-    }
+	}
+	
+	public get length() {
+		return this._length;
+	}
+
+	public set length(length: number) {
+		this._length = length
+	}
 
 }
