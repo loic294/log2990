310bdbbdf86b64e81fce7e097d906900e57e5838
Julien Bergeron <bergeron_julien@outlook.com>
2018-02-09 10:27:19 -0500

    Removed grid generator

--- server/app/routes/grid/middleware.ts
+++ server/app/routes/grid/middleware.ts
@@ -1,13 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-import GridGeneratorService from '../../services/gridGenerator'
-
 // Test lexical service
 export async function generate(req: Request, res: Response, next: NextFunction) {
-
-	const gridGeneratorService = new GridGeneratorService()
-
-	const grid = await gridGeneratorService.generate()
-
-	res.json({ grid })
+	res.json({ success: true })
 }
