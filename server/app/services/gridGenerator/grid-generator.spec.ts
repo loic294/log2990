5097bf43131b3ee97e4830a3f9928904d36e616d
Camille Bm <camille.bergeron-miron@polymtl.ca>
2018-02-09 10:29:23 -0500

    Corrections lint
    gridgenerator specs

--- server/app/services/gridGenerator/grid-generator.spec.ts
+++ server/app/services/gridGenerator/grid-generator.spec.ts
@@ -2,7 +2,9 @@ import GridGenerator from "./grid-generator";
 import { Case } from "../../../../common/grid/case";
 import {expect} from "chai";
 
-const assert = require("assert");
+/* tslint:disable:no-magic-numbers */
+/* tslint:disable:no-inferrable-types */
+/* tslint:disable:prefer-for-of */
 
 describe("GridGenerator", () => {
   let expectedTestArray: Array<Array<Case>>;
@@ -13,16 +15,16 @@ describe("GridGenerator", () => {
   it("should create a 10x10 empty array of cases", () => {
     actualTestArray = gridGenerator.createEmptyGrille();
     expectedTestArray = [
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case(),new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case(), new Case()],
     ];
 
     expect(actualTestArray).to.eql(expectedTestArray);
@@ -31,9 +33,9 @@ describe("GridGenerator", () => {
 
   it("should have " + gridGenerator.getAmntBlckCases() + " black cases", () => {
 
-    for(let rows: number = 0; rows < gridGenerator.getGrid().length; rows ++) {
+    for (let row: number = 0; row < gridGenerator.getGrid().length; row ++) {
       for (let col: number = 0; col < gridGenerator.getGrid().length; col ++) {
-        if (gridGenerator.getGrid()[rows][col].isBlack()) {
+        if (gridGenerator.getGrid()[row][col].isBlack()) {
           testAmntOfBlacks++;
         }
       }
@@ -43,9 +45,9 @@ describe("GridGenerator", () => {
 
   it("should have no invalid locations. (a white case surrounded by black cases)", () => {
     actualTestArray = [
-      [new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case()],
+      [new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case()],
     ];
 
     actualTestArray[0][1].setBlack(true);
@@ -54,16 +56,17 @@ describe("GridGenerator", () => {
     actualTestArray[1][2].setBlack(true);
 
     let nmbrBlackCases: number = 4;
+    const expectedValue: number = 3;
 
     nmbrBlackCases = gridGenerator.testCheckGrilleValidity(nmbrBlackCases, actualTestArray);
-    expect(nmbrBlackCases).to.equal(3);
+    expect(nmbrBlackCases).to.equal(expectedValue);
   });
 
   it("should have no invalid locations. (a white case on the right side surrounded by black cases)", () => {
     actualTestArray = [
-      [new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case()],
+      [new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case()],
     ];
 
     actualTestArray[0][2].setBlack(true);
@@ -71,16 +74,17 @@ describe("GridGenerator", () => {
     actualTestArray[2][2].setBlack(true);
 
     let nmbrBlackCases: number = 3;
+    const expectedValue: number = 2;
 
     nmbrBlackCases = gridGenerator.testCheckGrilleValidity(nmbrBlackCases, actualTestArray);
-    expect(nmbrBlackCases).to.equal(2);
+    expect(nmbrBlackCases).to.equal(expectedValue);
   });
 
   it("should have no invalid locations. (a white case on the bottom surrounded by black cases)", () => {
     actualTestArray = [
-      [new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case()],
-      [new Case(),new Case(),new Case()],
+      [new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case()],
+      [new Case(), new Case(), new Case()],
     ];
 
     actualTestArray[1][1].setBlack(true);
@@ -88,9 +92,10 @@ describe("GridGenerator", () => {
     actualTestArray[2][2].setBlack(true);
 
     let nmbrBlackCases: number = 3;
+    const expectedValue: number = 2;
 
     nmbrBlackCases = gridGenerator.testCheckGrilleValidity(nmbrBlackCases, actualTestArray);
-    expect(nmbrBlackCases).to.equal(2);
+    expect(nmbrBlackCases).to.equal(expectedValue);
   });
 
   it("should be filled with letters except for the black ones");
