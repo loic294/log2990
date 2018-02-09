2314e0c35530643a741fff9e1bbe39d286cd7ec4
Gabriel <gabcj29@hotmail.com>
2018-02-09 09:28:08 -0500

    Creation du component TrackCreation

---
+++ client/src/app/race/track-creation/track-creation.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TrackCreationComponent } from './track-creation.component';
+
+describe('TrackCreationComponent', () => {
+  let component: TrackCreationComponent;
+  let fixture: ComponentFixture<TrackCreationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TrackCreationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TrackCreationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/race/track-creation/track-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-track-creation',
+  templateUrl: './track-creation.component.html',
+  styleUrls: ['./track-creation.component.css']
+})
+export class TrackCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
