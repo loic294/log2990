2391f140d1bd15c5cdb9fa8d96e1a572f7868a92
Camille Bm <camille.bergeron-miron@polymtl.ca>
2018-02-19 09:50:06 -0500

    Revert commit models

--- server/app/models/game.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from "mongoose";
-
-const gameSchema: mongoose.Schema = new mongoose.Schema({
-    gameId: String,
-    players: Number,
-    difficulty: String
-});
-
-export const gameModel: mongoose.Model<mongoose.Document> = mongoose.model("gameModel", gameSchema);
