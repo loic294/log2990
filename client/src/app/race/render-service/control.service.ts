7dd09b6e8799bce9a031829bca223b19dcc34879
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2018-02-19 18:20:12 -0500

    Constructeur ajouté pour le service de contrôle.

--- client/src/app/race/render-service/control.service.ts
+++ client/src/app/race/render-service/control.service.ts
@@ -1,6 +1,11 @@
 import Serviceable from "./serviceable";
 
 export default class ControlService implements Serviceable {
+
+    public constructor() {
+
+    }
+
     public execute(): void {
 
     }
