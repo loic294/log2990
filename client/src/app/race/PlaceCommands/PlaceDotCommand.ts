bef17cfc2e1a9c35f16e72a03d3462c4b1c3fe1c
Gabriel <gabcj29@hotmail.com>
2018-02-19 21:14:12 -0500

    Completion cycle quand on remet un point sur le premier. Resize event sur window resize. Debut de drag event.

--- client/src/app/race/PlaceCommands/PlaceDotCommand.ts
+++ client/src/app/race/PlaceCommands/PlaceDotCommand.ts
@@ -1,11 +1,10 @@
 
 import { PlaceCommand } from "./PlaceCommand";
 
-import { Vector3, PointsMaterial, Points, Geometry, LineBasicMaterial, Line, Object3D, /*Raycaster, Intersection*/ } from "three";
+import { Vector3, PointsMaterial, Points, Geometry, LineBasicMaterial, Line, Object3D } from "three";
 
 const TWO: number = 2; // a changer
-const magicNumberX: number = 1288.194; // a changer
-const magicNumberY: number = 1292.105; // a changer
+const POINT_SIZE: number = 2;
 
 export class PlaceDotCommand extends PlaceCommand {
 
@@ -14,24 +13,26 @@ export class PlaceDotCommand extends PlaceCommand {
 
     public place(scene: THREE.Scene, renderer: THREE.WebGLRenderer, event: MouseEvent): void {
         // Methode temporaire (activer le click seulement sur objet)
-        if (event.srcElement === renderer.domElement) {
+
+        /*if (event.srcElement === renderer.domElement) {*/
+
             if (!this._isCycle) {
                 const dotGeo: THREE.Geometry = new Geometry();
                 this.findCoordinates(event.offsetX, event.offsetY, renderer);
                 dotGeo.vertices.push(new Vector3(this._dotMemory[this._dotMemory.length - 1].x,
-                    this._dotMemory[this._dotMemory.length - 1].y,
-                    this._dotMemory[this._dotMemory.length - 1].z));
-                const dotMat: THREE.PointsMaterial = (this._dotMemory.length === 1) ? new PointsMaterial({ color: 0xFFFFFF, size: 1 }) :
-                    new PointsMaterial({ color: 0x0000FF, size: 1 });
+                                                 this._dotMemory[this._dotMemory.length - 1].y,
+                                                 this._dotMemory[this._dotMemory.length - 1].z));
+                const dotMat: THREE.PointsMaterial = (this._dotMemory.length === 1) ?
+                                                    new PointsMaterial({ color: 0xFFFFFF, size: POINT_SIZE }) :
+                                                    new PointsMaterial({ color: 0x0000FF, size: POINT_SIZE });
                 const dot: THREE.Points = new Points(dotGeo, dotMat);
                 dot.name = "dot" + this._dotMemory.length;
                 scene.add(dot);
 
                 this.updateLines(scene);
-
-                this.findIfCycle();
+                this.findIfCycle(scene);
             }
-        }
+       // }
     }
 
     private findCoordinates(offsetX: number, offsetY: number, renderer: THREE.WebGLRenderer): void {
@@ -40,15 +41,13 @@ export class PlaceDotCommand extends PlaceCommand {
         const tempX: number = offsetX - (canvas.clientWidth / TWO);
         const tempY: number = offsetY - (canvas.clientHeight / TWO);
         const vec: Vector3 = new Vector3((canvas.clientWidth / canvas.clientHeight) * tempX * this._distanceZ / canvas.clientWidth,
-             0, tempY * this._distanceZ / canvas.clientHeight);
+                                         0, tempY * this._distanceZ / canvas.clientHeight);
         this._dotMemory.push(vec);
-        console.log("Offset: " + offsetX + ";" + offsetY);
-        console.log("Modified: " + vec.x + " ; " + vec.y + " ; " + vec.z);
     }
 
     private updateLines(scene: THREE.Scene): void {
         if (this._dotMemory.length > 1) {
-            const lineMat: THREE.LineBasicMaterial = new LineBasicMaterial({ color: 0xFF0000, linewidth: 10 });
+            const lineMat: THREE.LineBasicMaterial = new LineBasicMaterial({ color: 0xFF0000, linewidth: 8 });
             const lineGeo: THREE.Geometry = new Geometry();
             lineGeo.vertices.push(this._dotMemory[this._dotMemory.length - 1]);
             lineGeo.vertices.push(this._dotMemory[this._dotMemory.length - TWO]);
@@ -56,31 +55,35 @@ export class PlaceDotCommand extends PlaceCommand {
             line.name = "line" + (this._dotMemory.length - 1);
             scene.add(line);
         }
-
     }
 
-    /*
-    * Ineficace mais marche si on clique directement dessus au millimetre pres. A changer.
-    */
-    private findIfCycle(): void {
-        for (let i: number = 0; i < this._dotMemory.length - 1; ++i) {
-            if (this._dotMemory[i].x === this._dotMemory[this._dotMemory.length - 1].x &&
-                this._dotMemory[i].z === this._dotMemory[this._dotMemory.length - 1].z) {
-                this._isCycle = true;
-            }
+    private findIfCycle(scene: THREE.Scene): void {
+        if (this._dotMemory.length !== 1 && this.pointsRadiusCollide()) {
+             this._isCycle = true;
         }
     }
 
+    private pointsRadiusCollide(): boolean {
+        return Math.sqrt(Math.pow(this._dotMemory[0].x - this._dotMemory[this._dotMemory.length - 1].x, TWO) +
+               Math.pow(this._dotMemory[0].z - this._dotMemory[this._dotMemory.length - 1].z, TWO)) < POINT_SIZE / TWO;
+    }
+
     public undo(scene: THREE.Scene): void {
-        const dotName: string = "dot" + this._dotMemory.length;
-        const lineName: string = "line" + (this._dotMemory.length - 1);
+        if (!this._isCycle) {
+            const dotName: string = "dot" + this._dotMemory.length;
+            const lineName: string = "line" + (this._dotMemory.length - 1);
 
-        const dot: Object3D = scene.getObjectByName(dotName);
-        scene.remove(dot);
+            const dot: Object3D = scene.getObjectByName(dotName);
+            scene.remove(dot);
 
-        const line: Object3D = scene.getObjectByName(lineName);
-        scene.remove(line);
+            const line: Object3D = scene.getObjectByName(lineName);
+            scene.remove(line);
 
-        this._dotMemory.pop();
+            this._dotMemory.pop();
+        }
+    }
+
+    public dragDot(event: DragEvent, renderer: THREE.WebGLRenderer): void {
+
     }
 }
