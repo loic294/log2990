269c39bf541c2413db6c684876e0ca2cf1bcb898
Gabriel <gabcj29@hotmail.com>
2018-02-19 21:14:12 -0500

    Completion cycle quand on remet un point sur le premier. Resize event sur window resize. Debut de drag event.

--- client/src/app/race/PlaceCommands/PlaceCommand.ts
+++ client/src/app/race/PlaceCommands/PlaceCommand.ts
@@ -5,5 +5,7 @@ export class PlaceCommand {
 
     public place(scene: THREE.Scene, renderer: THREE.WebGLRenderer, event: Event): void {}
 
+    public dragDot(event: DragEvent, renderer: THREE.WebGLRenderer): void {}
+
     public undo(scene: THREE.Scene): void {}
 }
--- client/src/app/race/track-creation/track-creation.component.ts
+++ client/src/app/race/track-creation/track-creation.component.ts
@@ -1,12 +1,13 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, HostListener } from "@angular/core";
-import { OrthographicCamera, WebGLRenderer, Scene, GridHelper, AxisHelper, Vector3 } from "three";
+import { OrthographicCamera, WebGLRenderer, Scene, /* GridHelper, AxisHelper, */ Vector3 } from "three";
 import { PlaceCommand } from "../PlaceCommands/PlaceCommand";
 import { PlaceDotCommand } from "../PlaceCommands/PlaceDotCommand";
 
 const FAR_CLIPPING_PLANE: number = 10000;
-const NEAR_CLIPPING_PLANE: number = 1;
-const FIELD_OF_VIEW: number = 75;
+const NEAR_CLIPPING_PLANE: number = 0;
 const CAMERA_DISTANCE: number = 50;
+const ASPECT: number = window.innerWidth / window.innerHeight;
+const TWO: number = 2;
 
 const LEFT_CLICK: number = 1;
 const RIGHT_CLICK: number = 3;
@@ -20,9 +21,6 @@ export class TrackCreationComponent implements AfterViewInit {
 
     private _scene: THREE.Scene = new Scene();
     private _camera: THREE.OrthographicCamera;
-    /*private _camera: THREE.PerspectiveCamera = new PerspectiveCamera(
-        FIELD_OF_VIEW, window.innerWidth / window.innerHeight,
-        NEAR_CLIPPING_PLANE, FAR_CLIPPING_PLANE);*/
     private _renderer: THREE.WebGLRenderer = new WebGLRenderer();
     private _dotCommand: PlaceCommand = new PlaceDotCommand(CAMERA_DISTANCE);
 
@@ -30,6 +28,22 @@ export class TrackCreationComponent implements AfterViewInit {
     private container: ElementRef;
 
     public constructor() { }
+
+    @HostListener("window:drag", ["event"])
+    public dragDot(event: DragEvent): void {
+        this._dotCommand.dragDot(event, this._renderer);
+        this.render();
+    }
+
+    @HostListener("window:resize", ["$event"])
+    public onResize(): void {
+        this._camera.left = CAMERA_DISTANCE * ASPECT / - TWO;
+        this._camera.right = CAMERA_DISTANCE * ASPECT / TWO;
+        this._camera.top = CAMERA_DISTANCE / TWO;
+        this._camera.bottom = CAMERA_DISTANCE / - TWO;
+        this._camera.updateProjectionMatrix();
+        this._renderer.setSize(window.innerWidth, window.innerHeight);
+    }
 
     @HostListener("window:click", ["$event"])
     public onKeyUp(event: MouseEvent): void {
@@ -56,17 +70,16 @@ export class TrackCreationComponent implements AfterViewInit {
 
     public ngAfterViewInit(): void {
         // Debug help
-        const gridHelper: THREE.GridHelper = new GridHelper(10, 10);
+        /*const gridHelper: THREE.GridHelper = new GridHelper(10, 10);
         const axes: THREE.AxisHelper = new AxisHelper(2);
         this._scene.add(axes);
         gridHelper.position.set(0, 0, 0);
         gridHelper.scale.set(2, 2, 2);
-        this._scene.add(gridHelper);
+        this._scene.add(gridHelper);*/
         /////////////////////////////////////////////////////////////////
 
-        const aspect: number = window.innerWidth / window.innerHeight;
-        this._camera = new OrthographicCamera(
-            CAMERA_DISTANCE * aspect / - 2, CAMERA_DISTANCE * aspect / 2, CAMERA_DISTANCE / 2, CAMERA_DISTANCE / - 2, 0, 10000);
+        this._camera = new OrthographicCamera(CAMERA_DISTANCE * ASPECT / - TWO, CAMERA_DISTANCE * ASPECT / TWO,
+                                              CAMERA_DISTANCE / TWO, CAMERA_DISTANCE / - TWO, NEAR_CLIPPING_PLANE, FAR_CLIPPING_PLANE);
 
         this._camera.position.set(0, CAMERA_DISTANCE , 0);
         this._camera.lookAt(new Vector3(0, 0, 0));
