b172587c674da13dbb63b47c39f8cbd93c235509
nickbyd17 <nbayard17@gmail.com>
2018-02-25 19:17:01 -0500

    routing problem solved

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CrosswordComponent } from "./crosswords/crossword/crossword.component";
 import { GameComponent } from "./race/game-component/game.component";
+import { TrackCreationComponent } from "./race/track-creation/track-creation.component";
 
 const routes: Routes = [
     { path: "crossword", component: CrosswordComponent },
     { path: "race", component: GameComponent }
+    { path: "race/admin", component: TrackCreationComponent  }
 ];
 
 @NgModule({
