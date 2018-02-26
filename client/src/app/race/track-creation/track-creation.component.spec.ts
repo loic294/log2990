4ea7a9ce2c8b5691bd307af268803f4354a7e93c
Gabriel <gabcj29@hotmail.com>
2018-02-26 15:21:14 -0500

    Unit tests track creation

--- client/src/app/race/track-creation/track-creation.component.spec.ts
+++ client/src/app/race/track-creation/track-creation.component.spec.ts
@@ -1,13 +1,10 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { By } from "@angular/platform-browser";
 import { TrackCreationComponent } from "./track-creation.component";
-import { DebugElement } from "@angular/core";
 import { Object3D, Line } from "three";
 
 describe("TrackCreationComponent", () => {
     let component: TrackCreationComponent;
     let fixture: ComponentFixture<TrackCreationComponent>;
-    let debugElement: DebugElement;
     let scene: THREE.Scene;
     let vertices: Array<Object3D>;
     let edges: Array<Line>;
@@ -27,8 +24,6 @@ describe("TrackCreationComponent", () => {
         scene = component.getScene();
         vertices = component.getDotCommand().getVertices();
         edges = component.getDotCommand().getEdges();
-        tackIsCompleted = component.getDotCommand().getTrackIsCompleted();
-        debugElement = fixture.debugElement.query(By.all());
     });
 
     it("should create", () => {
@@ -40,28 +35,63 @@ describe("TrackCreationComponent", () => {
     });
 
     it("should place a dot in scene", () => {
-        debugElement.triggerEventHandler("mousedown", { offsetX: 1, offsetY: 1 });
-        expect(scene.children.length).toBe(1);
+        const event: MouseEvent = new MouseEvent("mousedown");
+        component.onKeyDown(event);
+        expect(scene.children.length).toBe(2);
         expect(vertices.length).toBe(1);
     });
 
     it("should place two dots and a line in scene", () => {
-        debugElement.triggerEventHandler("mousedown", { offsetX: 1, offsetY: 1 });
-        debugElement.triggerEventHandler("mousedown", { offsetX: 2, offsetY: 2 });
-        expect(scene.children.length).toBe(3);
+
+        const eventDown1: MouseEvent = new MouseEvent("mousedown", {clientX: 100, clientY: 50});
+        const eventDown2: MouseEvent = new MouseEvent("mousedown", {clientX: 200, clientY: 100});
+        const eventUp: MouseEvent = new MouseEvent("mouseup");
+
+        component.onKeyDown(eventDown1);
+        component.onKeyUp(eventUp);
+        component.onKeyDown(eventDown2);
+
+        expect(scene.children.length).toBe(4);
         expect(vertices.length).toBe(2);
         expect(edges.length).toBe(1);
     });
 
-    it("should place three dots and three lines in scene and should complete track", () => {
-        debugElement.triggerEventHandler("mousedown", { pageX: 1, pageY: 1 });
-        debugElement.triggerEventHandler("mousedown", { pageX: 2, pageY: 2 });
-        debugElement.triggerEventHandler("mousedown", { pageX: 3, pageY: 3 });
-        debugElement.triggerEventHandler("mousedown", { pageX: 1, pageY: 1 });
-        expect(scene.children.length).toBe(6);
+    it("should place three dots and three lines in scene, should complete track and should not add 4th dot.", () => {
+        const eventDown1: MouseEvent = new MouseEvent("mousedown", {clientX: 100, clientY: 50});
+        const eventUp: MouseEvent = new MouseEvent("mouseup");
+        component.onKeyDown(eventDown1);
+        component.onKeyUp(eventUp);
+        const eventDown2: MouseEvent = new MouseEvent("mousedown", {clientX: 200, clientY: 100});
+        component.onKeyDown(eventDown2);
+        component.onKeyUp(eventUp);
+        const eventDown3: MouseEvent = new MouseEvent("mousedown", {clientX: 300, clientY: 50});
+        component.onKeyDown(eventDown3);
+        component.onKeyUp(eventUp);
+        component.onKeyDown(eventDown1);
+        component.onKeyUp(eventUp);
+        const eventDown4: MouseEvent = new MouseEvent("mousedown", {clientX: 150, clientY: 150});
+        component.onKeyDown(eventDown4);
+        component.onKeyUp(eventUp);
+
+        expect(scene.children.length).toBe(7);
         expect(vertices.length).toBe(3);
         expect(edges.length).toBe(3);
-        expect(tackIsCompleted).toBe(true);
+        expect(component.getDotCommand().getTrackIsCompleted()).toBe(true);
     });
+
+    it("should place a single dot, no line and track should not be completed", () => {
+        const eventDown1: MouseEvent = new MouseEvent("mousedown", {clientX: 100, clientY: 50});
+        const eventUp: MouseEvent = new MouseEvent("mouseup");
+        component.onKeyDown(eventDown1);
+        component.onKeyUp(eventUp);
+        component.onKeyDown(eventDown1);
+        component.onKeyUp(eventUp);
+
+        expect(scene.children.length).toBe(2);
+        expect(vertices.length).toBe(1);
+        expect(edges.length).toBe(0);
+        expect(component.getDotCommand().getTrackIsCompleted()).toBe(false);
+    });
+
 
 });
