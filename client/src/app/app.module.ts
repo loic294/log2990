3dac6a93b9ea57f1d8db09f75aafe2617dac07a2
Julien Bergeron <bergeron_julien@outlook.com>
2018-03-01 22:34:29 -0500

    pipeline config

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -41,13 +41,9 @@ const config: SocketIoConfig = { url: "http://localhost:3000", options: {} };
         ModeComponent,
         ModeDialogComponent,
         HomeComponent,
-<<<<<<< HEAD
-        TrackCreationComponent
-=======
         TrackCreationComponent,
         ModeDialogComponent,
         DisconnectedDialogComponent
->>>>>>> added opponent disconnected warning
     ],
     entryComponents: [
         ModeDialogComponent,
