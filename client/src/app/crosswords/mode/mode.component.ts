4621ea635ed847ba27e2feb30456d577b0a11c54
Camille Bm <camille.bergeron-miron@polymtl.ca>
2018-03-07 15:55:08 -0500

    Changed initial values of mode component attributes

--- client/src/app/crosswords/mode/mode.component.ts
+++ client/src/app/crosswords/mode/mode.component.ts
@@ -10,9 +10,9 @@ import {MatDialogRef, MatDialog, MAT_DIALOG_DATA} from "@angular/material";
   })
   export class ModeDialogComponent {
 
-    public showDifficulty: boolean = true;
-    public showNameInput: boolean = true;
-    public showStartSoloGame: boolean = true;
+    public showDifficulty: boolean = false;
+    public showNameInput: boolean = false;
+    public showStartSoloGame: boolean = false;
     public waitingForPlayer: boolean = false;
 
     public constructor (
@@ -74,7 +74,7 @@ import {MatDialogRef, MatDialog, MAT_DIALOG_DATA} from "@angular/material";
     }
 
     public startSoloGame(): boolean {
-        if (this.selectedMode === "Single Player" && !this.showNameInput) {
+        if (this.selectedMode === "Single Player" && this.showNameInput) {
             return true;
         }
 
@@ -82,11 +82,11 @@ import {MatDialogRef, MatDialog, MAT_DIALOG_DATA} from "@angular/material";
     }
 
     public isDifficultySelected(): boolean {
-       return this.showNameInput = false;
+       return this.showNameInput = true;
     }
 
     public isMultiPlayer(): boolean {
-        if (!this.showNameInput && this.selectedMode === "Two Players") {
+        if (this.showNameInput && this.selectedMode === "Two Players") {
             return true;
         }
 
@@ -111,7 +111,7 @@ import {MatDialogRef, MatDialog, MAT_DIALOG_DATA} from "@angular/material";
     }
 
     public onSelect(mode: string): void {
-        this.showDifficulty = false;
+        this.showDifficulty = true;
 
         return this.socketService.onSelect(mode);
 
@@ -169,7 +169,7 @@ import {MatDialogRef, MatDialog, MAT_DIALOG_DATA} from "@angular/material";
       public ngOnInit(): void {}
 
 }
-
+// tslint:disable max-classes-per-file
 @Component({
     selector: "app-mode",
     templateUrl: "./mode.component.html",
