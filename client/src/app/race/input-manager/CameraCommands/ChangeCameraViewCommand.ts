f425d00bc3f30008d818887ce3741ace6c114bb5
nickbyd17 <nbayard17@gmail.com>
2018-03-10 00:33:33 -0500

    error correction

--- client/src/app/race/input-manager/CameraCommands/ChangeCameraViewCommand.ts
+++ client/src/app/race/input-manager/CameraCommands/ChangeCameraViewCommand.ts
@@ -1,11 +1,7 @@
 import AbsCameraCommand from "./AbsCameraCommand";
 
-enum CAMERA_VIEW {
-    TOP_DOWN = 0,
-    THIRD_PERSON = 1
-}
-
 export default class ChangeCameraViewCommand extends AbsCameraCommand {
+
     public subscribe(): void {
         this.renderer.changeCamera();
     }
