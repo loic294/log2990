3af245f67e162c89e4b7c3960d105dd022d712c0
Gabriel <gabcj29@hotmail.com>
2018-03-18 16:00:12 -0400

    POST et GET fonctionnels pour structure ITrackInfo

--- server/app/models/trackInfo.ts
+++ server/app/models/trackInfo.ts
@@ -1,12 +1,12 @@
 
-import { Document, Schema} from "mongoose";
+import { Document, Schema } from "mongoose";
 import mongoose from "../config/mongoose";
 
 export interface ITrack {
-  name?: String;
-  type?: String;
-  description?: String;
-  timesPlayed?: number;
+    name?: String;
+    type?: String;
+    description?: String;
+    timesPlayed?: number;
 }
 
 export interface ITrackInfo extends Document {
@@ -17,10 +17,10 @@ export interface ITrackInfo extends Document {
 }
 
 export const trackSchema: Schema = new Schema({
-  name: String,
-  createdAt: String,
-  players: String,
-  difficulty: String
+    name: String,
+    type: String,
+    description: String,
+    timesPlayed: Number
 });
 
-export default mongoose.model<ITrackInfo>("tracks", trackSchema);
+export default mongoose.model<ITrackInfo>("Track", trackSchema);
--- server/app/routes/trackInformation/middleware.ts
+++ server/app/routes/trackInformation/middleware.ts
@@ -1,12 +1,33 @@
 import { Request, Response, NextFunction } from "express";
+import Track, { ITrackInfo } from "../../models/trackInfo";
 
 const ERR_500: number = 500;
 
 export const obtainTracks: (req: Request, res: Response, next: NextFunction) => Promise<void> =
     async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
+        const track: ITrackInfo[] = await Track.find({name: "trackName", type: "trackType"},
+                                                     {_id: 0, name: 1, type: 1, description: 1, timesPlayed: 1});
         try {
-            res.send("Hello World!");
+            res.json(track[0]);
+        } catch (err) {
+            res.status(ERR_500).send(err.message);
+        }
+};
+
+export const saveTrack: (req: Request, res: Response, next: NextFunction) => Promise<void> =
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
+        const track: ITrackInfo = new Track({
+            name: "trackName",
+            type: "trackType",
+            description: "a track",
+            timesPlayed: 0
+        });
+        await track.save();
+
+        try {
+            res.send("Hello POST!");
         } catch (err) {
             res.status(ERR_500).send(err.message);
         }
