c33917f7bd0274db122fe3f45ec6e70f3da7b751
Julien Bergeron <bergeron_julien@hotmail.com>
2018-03-20 11:12:44 -0400

    highligth borders works

--- client/src/app/crosswords/grid/grid.component.ts
+++ client/src/app/crosswords/grid/grid.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { Cell } from "../../../../../common/grid/cell";
 import { GridService } from "../../grid.service/grid.service";
+import { Orientation } from "../../../../../common/lexical/word";
 
 /** TEMPORARY MOCKED CONTENT
    * Example table
@@ -41,5 +42,29 @@ export class GridComponent implements OnInit {
         this._gridService.selectCaseFromGrid(c);
     }
 
+    public highligthStyle(): any {
+        const wordTotal: number = this._gridService.wordLength() * 50;
+        console.log(wordTotal);
+        let abx: string;
+        abx = wordTotal.toString();
+        console.log(abx);
+        abx += "px";
+        console.log(abx);
+
+        // tslint:disable-next-line:typedef
+        // tslint:disable-next-line:no-unnecessary-local-variable
+        const styles = {
+            // "background-color": this.user.isExpired ? "red" : "transparent",
+            // "font-weight": this.isImportant ? "bold" : "normal"
+            // "height": "50px",
+            "height": !this._gridService.isVertical() ? "50px" : abx,
+            "width": this._gridService.isVertical() ? "50px" : abx
+          };
+
+          console.log
+
+        return styles;
+
+    }
     public ngOnInit(): void {}
 }
