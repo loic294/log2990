12d6f3bc30a88defd4c9b3334341ebfe28a9d5b5
Julien Bergeron <bergeron_julien@hotmail.com>
2018-03-20 11:34:31 -0400

    highligth bug fix

--- client/src/app/crosswords/grid/grid.component.ts
+++ client/src/app/crosswords/grid/grid.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from "@angular/core";
 import { Cell } from "../../../../../common/grid/cell";
 import { GridService } from "../../grid.service/grid.service";
 import { Orientation } from "../../../../../common/lexical/word";
+import { NgStyle } from "@angular/common";
 
 /** TEMPORARY MOCKED CONTENT
    * Example table
@@ -42,29 +43,21 @@ export class GridComponent implements OnInit {
         this._gridService.selectCaseFromGrid(c);
     }
 
-    public highligthStyle(): any {
-        const wordTotal: number = this._gridService.wordLength() * 50;
-        console.log(wordTotal);
-        let abx: string;
-        abx = wordTotal.toString();
-        console.log(abx);
-        abx += "px";
-        console.log(abx);
+    public highligthStyle(): {} {
+        const cellHeigth: number = 50;
+        const wordTotal: number = this._gridService.wordLength() * cellHeigth;
+        let formatedNumber: string;
+        formatedNumber = wordTotal.toString();
+        formatedNumber += "px";
 
-        // tslint:disable-next-line:typedef
         // tslint:disable-next-line:no-unnecessary-local-variable
-        const styles = {
-            // "background-color": this.user.isExpired ? "red" : "transparent",
-            // "font-weight": this.isImportant ? "bold" : "normal"
-            // "height": "50px",
-            "height": !this._gridService.isVertical() ? "50px" : abx,
-            "width": this._gridService.isVertical() ? "50px" : abx
+        const styles: {} = {
+            "height": !this._gridService.isVertical() ? "50px" : formatedNumber,
+            "width": this._gridService.isVertical() ? "50px" : formatedNumber
           };
 
-          console.log
-
         return styles;
-
     }
+    
     public ngOnInit(): void {}
 }
