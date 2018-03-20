1454b61cd2e1765eb5c6da5cb82bce6262656cd9
Julien Bergeron <bergeron_julien@hotmail.com>
2018-03-20 18:46:33 -0400

    all crossword tests seems to be passing

--- client/src/app/crosswords/termination/termination.component.spec.ts
+++ client/src/app/crosswords/termination/termination.component.spec.ts
@@ -1,8 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// tslint:disable
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TerminationComponent } from './termination.component';
+import { TerminationComponent } from "./termination.component";
 
-describe('TerminationComponent', () => {
+describe("TerminationComponent", () => {
   let component: TerminationComponent;
   let fixture: ComponentFixture<TerminationComponent>;
 
@@ -19,7 +20,7 @@ describe('TerminationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
