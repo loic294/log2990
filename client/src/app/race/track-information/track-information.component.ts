d32d2709504d54b4a3296143adadd54737b70349
Gabriel <gabcj29@hotmail.com>
2018-03-21 20:31:51 -0400

    Utilisateur ne peut plus modifier les pistes

--- client/src/app/race/track-information/track-information.component.ts
+++ client/src/app/race/track-information/track-information.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
-import { ITrackInfo } from "../../../../../server/app/models/trackInfo";
+import { ITrack } from "../../../../../server/app/models/trackInfo";
 import { TrackInformationService } from "../../../../../server/app/services/trackInformation/trackInformationService";
 
 @Component({
@@ -11,19 +11,10 @@ import { TrackInformationService } from "../../../../../server/app/services/trac
 export class TrackInformationComponent implements OnInit {
 
     private _tracks: Array<String>;
-    private _currentTrack: ITrackInfo;
-    private _name: String;
-    private _type: String;
-    private _description: String;
-    private _timesPlayed: number;
-    private _vertice: Array<Array<Number>>;
+    private _currentTrack: ITrack;
 
     public constructor(private _trackInfo: TrackInformationService) {
         this._tracks = new Array();
-        this._name = "";
-        this._type = "";
-        this._description = "";
-        this._timesPlayed = 0;
     }
 
     public getTracksList(): void {
@@ -36,58 +27,11 @@ export class TrackInformationComponent implements OnInit {
 
     public getTrackInfo(trackName: String): void {
         this._trackInfo.getTracks(trackName).then((data) => {
-            const tempArray: Array<ITrackInfo> = JSON.parse(data.toString());
+            const tempArray: Array<ITrack> = JSON.parse(data.toString());
             this._currentTrack = tempArray[0];
         }).catch((error) => {
             throw error;
         });
-    }
-
-    public putTrack(): void {
-        const track: Object = {name: this._name, type: this._type, description: this._description,
-                               timesPlayed: this._timesPlayed, vertice: this._vertice};
-        this._trackInfo.putTrack(track).then(() => {
-            this.getTracksList();
-        }).catch((error) => {
-            throw error;
-        });
-    }
-
-    public deleteTrack(): void {
-        this._trackInfo.deleteTrack(this._currentTrack.name).then(() => {
-            this._currentTrack = null;
-            this.getTracksList();
-        }).catch((error) => {
-            throw error;
-        });
-    }
-
-    public set name(name: String) {
-        this._name = name;
-    }
-
-    public set type(type: String) {
-        this._type = type;
-    }
-
-    public set description(description: String) {
-        this._description = description;
-    }
-
-    public set timesPlayed(timesPlayed: number) {
-        this._timesPlayed = timesPlayed;
-    }
-
-    public set vertice(vertice: Array<Array<Number>>) {
-        this._vertice = vertice;
-    }
-
-    public get currentTrack(): ITrackInfo {
-        return this._currentTrack;
-    }
-
-    public get tracks(): Array<String> {
-        return this._tracks;
     }
 
     public ngOnInit(): void {
