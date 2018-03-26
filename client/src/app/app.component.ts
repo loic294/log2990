ba2a89372a845b3513c7bbca55b2bd70f0886baf
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-03-26 16:27:02 -0400

    Removed another OnInit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
     public readonly title: string = "LOG2990";
     public message: string;
