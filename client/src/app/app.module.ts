3aa2698621ab6bff6fd73ba7dcdda429f652a828
Julien Bergeron <bergeron_julien@hotmail.com>
2018-03-31 08:59:29 -0400

    ajout component HUD

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { HomeComponent } from "./home/home.component";
 import { TrackInformationService } from "../../../server/app/services/trackInformation/trackInformationService";
 import { TrackProgressionService } from "./race/trackProgressionService";
 import { TerminationComponent, TerminationDialogComponent } from "./crosswords/termination/termination.component";
+import { HudComponent } from './race/hud/hud.component';
 
 const config: SocketIoConfig = { url: "http://localhost:3000", options: {} };
 
@@ -47,7 +48,8 @@ const config: SocketIoConfig = { url: "http://localhost:3000", options: {} };
         TrackCreationComponent,
         TerminationComponent,
         TerminationDialogComponent,
-        ModeDialogComponent
+        ModeDialogComponent,
+        HudComponent
     ],
     entryComponents: [
         ModeDialogComponent,
