d05a7a69abb2172cc467b4633ae4c7b946e2c5f5
nickbyd17 <nbayard17@gmail.com>
2018-04-06 12:52:58 -0400

    spot light the track correctly, cleaning is neaded

--- client/src/app/race/car/headlights.ts
+++ client/src/app/race/car/headlights.ts
@@ -12,12 +12,20 @@ export default class Headlights extends Object3D {
     public constructor(carPosition: Vector3) {
         super();
         this._headlights = new Array<SpotLight>();
-        this._headlights.push(new SpotLight("0xffffff", 1, 10, 0.5, 1, 1));
-        this._headlights.push(new SpotLight("0xffffff", 1, 10, 0.5, 1, 1));
-        this._headlights[LIGHT_POSITION.FRONT_LEFT].position.copy(new Vector3(0, 3, 0));
-        this._headlights[LIGHT_POSITION.FRONT_LEFT].target.position.copy(new Vector3(-10, 0, 0));
+        this._headlights.push(new SpotLight("0xffffff", 2, 200, 0.6, 0.2, 1));
+        this._headlights.push(new SpotLight("0xffffff", 2, 100, 0.6, 0.2, 1));
+
+        this._headlights[LIGHT_POSITION.FRONT_LEFT].position.copy(new Vector3(0.4, 0.5, -1.3)); // pas touche
+        this._headlights[LIGHT_POSITION.FRONT_LEFT].target.position.copy(new Vector3(0, -1, -20));
+
+        this._headlights[LIGHT_POSITION.FRONT_RIGHT].position.copy(new Vector3(-0.4, 0.5, -1.3)); // pas touche
+        this._headlights[LIGHT_POSITION.FRONT_RIGHT].target.position.copy(new Vector3(0, -1, -20));
+
         this.add(this._headlights[0]);
         this.add(this._headlights[0].target);
+
+        this.add(this._headlights[1]);
+        this.add(this._headlights[1].target);
     }
 
     public get lights(): Array<SpotLight> {
