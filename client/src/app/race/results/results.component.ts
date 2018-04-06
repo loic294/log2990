d63d4e74f44189f36d187de2baef4817c81368ef
Camille Bm <camille.bergeron-miron@polymtl.ca>
2018-04-06 11:01:46 -0400

    sorted bestimes
    shows 5 best times of track

--- client/src/app/race/results/results.component.ts
+++ client/src/app/race/results/results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { TrackProgressionService, IGameInformation } from "../trackProgressionService";
 import { ResultsService } from "../results-service/results.service";
+// import { TrackInformationService } from "../../../../../server/app/services/trackInformation/trackInformationService";
 
 @Component({
     selector: "app-results",
@@ -13,27 +14,44 @@ import { ResultsService } from "../results-service/results.service";
 export class ResultsComponent implements OnInit {
     private _game: IGameInformation;
     private _isHidden: boolean;
+    private _bestTimes: Array<number>;
 
-    public constructor( private resultsService: ResultsService ) {
+    public constructor( private resultsService: ResultsService) {
         this._isHidden = true;
         this._game =  {gameTime: 0, lapTimes: new Array(), gameIsFinished: false, currentLap: 1};
     }
 
     public show(): void {
         this._isHidden = false;
-        this.times();
     }
 
     public get isHidden(): boolean {
         return this._isHidden;
     }
-    public times(): void {
-        console.log("game info", this._game.gameTime);
+    public bestTimes(): Array<number> {
+        return this._bestTimes;
     }
+
+    // public isBestTime(): boolean {
+    //     for (const time of this.bestTimes()) {
+    //         if (time === this._game.gameTime) {            
+    //             return true;
+    //         }
+    //     }
+
+    //     return false;
+    // }
     // tslint:disable-next-line:typedef
+
     public ngOnInit() {
         this.resultsService.game.subscribe( (game) => {
             this._game = game;
+        });
+        this.resultsService.trackTimes.subscribe((times) => {
+            const start: number = 0;
+            const end: number = 5;
+            const sortedTimes: Array<number> = times.sort((n1, n2) => n1 - n2).slice(start, end);
+            this._bestTimes = sortedTimes;
         });
     }
 
