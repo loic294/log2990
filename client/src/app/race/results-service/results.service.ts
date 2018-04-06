dd89ec8f52039117d094fa387a206235a5748d24
Camille Bm <camille.bergeron-miron@polymtl.ca>
2018-04-06 11:01:01 -0400

    get completedTimes result service methode

--- client/src/app/race/results-service/results.service.ts
+++ client/src/app/race/results-service/results.service.ts
@@ -9,8 +9,12 @@ export class ResultsService {
     private _game: Observable<IGameInformation>;
     private _gameSubject: Subject<IGameInformation> = new Subject<IGameInformation>();
 
+    private _times: Observable<Array<number>>;
+    private _timesSubject: Subject<Array<number>> = new Subject<Array<number>>();
+
     public constructor() {
         this._game = this._gameSubject.asObservable();
+        this._times = this._timesSubject.asObservable();
     }
 
     public get game(): Observable<IGameInformation> {
@@ -19,6 +23,14 @@ export class ResultsService {
 
     public selectGame(game: IGameInformation): void {
         this._gameSubject.next(game);
+    }
+
+    public get trackTimes(): Observable<Array<number>> {
+        return this._times;
+    }
+
+    public selectTrackTimes(times: Array<number>): void {
+        this._timesSubject.next(times);
     }
 
 }
