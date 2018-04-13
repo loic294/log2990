de9ccbece22fb57a3ca25442a5c180c385ae7a04
Loïc Bellemare-Alford <loicbellemare@hotmail.com>
2018-04-12 21:35:11 -0400

    Change to 10 x 10

--- server/app/routes/grid/middleware.ts
+++ server/app/routes/grid/middleware.ts
@@ -5,7 +5,7 @@ import GridGeneration from "../../services/gridGeneration/gridGeneration";
 export const generate: (req: Request, res: Response, next: NextFunction) => Promise<void> =
     async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
-    const GRID_SIZE: number = 8;
+    const GRID_SIZE: number = 10;
 
     const grid: GridGeneration = new GridGeneration();
     grid.initializeGrid(GRID_SIZE);
