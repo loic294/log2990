80537bf4c39537f43e73b1f01db49c69d8156732
Gabriel <gabcj29@hotmail.com>
2018-04-18 19:02:11 -0400

    fix pour dependance circulaire

--- client/src/app/race/car/resistance.ts
+++ client/src/app/race/car/resistance.ts
@@ -1,70 +1,85 @@
 import { Vector3 } from "three";
-import { Car , MINIMUM_SPEED, NUMBER_REAR_WHEELS, NUMBER_WHEELS } from "../car/car";
-import { GRAVITY } from "../../constants";
+import { GRAVITY, MINIMUM_SPEED, NUMBER_REAR_WHEELS, NUMBER_WHEELS } from "../../constants";
 
 const LATITUDINAL_COEFFICIENT: number = 0.3;
 
+export interface IResistanceParameters {
+    speed: Vector3;
+    direction: Vector3;
+    isGoingForward: boolean;
+    isGoingBackward: boolean;
+    isBraking: boolean;
+    isAcceleratorPressed: boolean;
+    brakeForce: Vector3;
+    up: Vector3;
+    mass: number;
+    weightRear: number;
+    frictionCoefficient: number;
+    engineForce: number;
+    dragCoefficient: number;
+}
+
 export class Resistance {
 
-    public static getResultingForce(car: Car): Vector3 {
+    public static getResultingForce(parameters: IResistanceParameters, dir: Vector3): Vector3 {
         const resultingForce: Vector3 = new Vector3();
 
-        if (car.speed.length() >= MINIMUM_SPEED) {
-            const dragForce: Vector3 = this.getDragForce(car);
-            const rollingResistance: Vector3 = this.getRollingResistance(car);
-            const latitudinalResistance: Vector3 = this.getLatitudinalResistance(car);
+        if (parameters.speed.length() >= MINIMUM_SPEED) {
+            const dragForce: Vector3 = this.getDragForce(parameters);
+            const rollingResistance: Vector3 = this.getRollingResistance(parameters);
+            const latitudinalResistance: Vector3 = this.getLatitudinalResistance(parameters);
             resultingForce.add(latitudinalResistance);
-            const direction: number = car.speed.dot(car.direction) / Math.abs(car.speed.dot(car.direction));
+            const direction: number = parameters.speed.dot(dir) / Math.abs(parameters.speed.dot(dir));
             resultingForce.add(dragForce.multiplyScalar(direction))
                 .add(rollingResistance.multiplyScalar(direction));
-
         }
-        if (car.isAcceleratorPressed) {
-            const tractionForce: number = this.getTractionForce(car);
-            const accelerationForce: Vector3 = car.direction;
+        if (parameters.isAcceleratorPressed) {
+            const tractionForce: number = this.getTractionForce(parameters);
+            const accelerationForce: Vector3 = dir;
             accelerationForce.multiplyScalar(tractionForce);
             resultingForce.add(accelerationForce);
-        } else if (car.isBraking && car.isGoingForward()) {
-            const brakeForce: Vector3 = car.getBrakeForce();
+        } else if (parameters.isBraking && parameters.isGoingForward) {
+            const brakeForce: Vector3 = parameters.brakeForce;
             resultingForce.add(brakeForce);
-        } else if (car.isBraking && car.isGoingBackward()) {
-            const brakeForce: Vector3 = car.getBrakeForce();
+        } else if (parameters.isBraking && parameters.isGoingBackward) {
+            const brakeForce: Vector3 = parameters.brakeForce;
             resultingForce.sub(brakeForce);
         }
 
         return resultingForce;
     }
 
-    private static getRollingResistance(car: Car): Vector3 {
+    private static getRollingResistance(parameters: IResistanceParameters): Vector3 {
         const tirePressure: number = 1;
         // formula taken from: https://www.engineeringtoolbox.com/rolling-friction-resistance-d_1303.html
 
         // tslint:disable-next-line:no-magic-numbers
-        const rollingCoefficient: number = (1 / tirePressure) * (Math.pow(car.speed.length() * 3.6 / 100, 2) * 0.0095 + 0.01) + 0.005;
+        const rollCoefficient: number = (1 / tirePressure) * (Math.pow(parameters.speed.length() * 3.6 / 100, 2) * 0.0095 + 0.01) + 0.005;
 
-        return car.direction.multiplyScalar(rollingCoefficient * car.mass * GRAVITY);
+        return parameters.direction.multiplyScalar(rollCoefficient * parameters.mass * GRAVITY);
     }
 
-    private static getLatitudinalResistance(car: Car): Vector3 {
-        const latitude: Vector3 = car.direction.cross(car.up);
-        const vLaterale: Vector3 = latitude.normalize().multiplyScalar(car.speed.dot(latitude));
+    private static getLatitudinalResistance(parameters: IResistanceParameters): Vector3 {
+        const latitude: Vector3 = parameters.direction.cross(parameters.up);
+        const vLaterale: Vector3 = latitude.normalize().multiplyScalar(parameters.speed.dot(latitude));
 
-        return vLaterale.multiplyScalar(LATITUDINAL_COEFFICIENT * car.mass * GRAVITY);
+        return vLaterale.multiplyScalar(LATITUDINAL_COEFFICIENT * parameters.mass * GRAVITY);
     }
 
-    private static getDragForce(car: Car): Vector3 {
+    private static getDragForce(parameters: IResistanceParameters): Vector3 {
         const carSurface: number = 3;
         const airDensity: number = 1.2;
-        const resistance: Vector3 = car.direction;
-        resistance.multiplyScalar(airDensity * carSurface * - car.dragCoefficient * car.speed.length() * car.speed.length());
+        const resistance: Vector3 = parameters.direction;
+        resistance.multiplyScalar(airDensity * carSurface * -
+            parameters.dragCoefficient * parameters.speed.length() * parameters.speed.length());
 
         return resistance;
     }
 
-    public static getTractionForce(car: Car): number {
-        const force: number = car.getEngineForce();
+    public static getTractionForce(parameters: IResistanceParameters): number {
+        const force: number = parameters.engineForce;
         const maxForce: number =
-            car.rearWheel.frictionCoefficient * car.mass * GRAVITY * car.weightRear * NUMBER_REAR_WHEELS / NUMBER_WHEELS;
+        parameters.frictionCoefficient * parameters.mass * GRAVITY * parameters.weightRear * NUMBER_REAR_WHEELS / NUMBER_WHEELS;
 
         return -Math.min(force, maxForce);
     }
