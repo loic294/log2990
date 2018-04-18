be642ed00d91f7e2e262a4274a2fa66e1039cd99
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2018-04-18 10:13:36 -0400

    Function created for the ai out of bounds, and cleaned up the code a little.

--- client/src/app/race/car/collision.ts
+++ client/src/app/race/car/collision.ts
@@ -1,12 +1,9 @@
 import { Vector3, Matrix4, Mesh, Raycaster, Intersection } from "three";
 import { Car } from "./car";
 
-const SPECIAL_COLOR: number = 0xFFFF00;
-const SPECIAL_COLOR_2: number = 0x888F00;
 const ABSOLUTE_CAR_LENGTH_X: number = -0.7128778274977209;
 const ABSOLUTE_CAR_LENGTH_Y: number = -0.007726105637907718;
 const ABSOLUTE_CAR_LENGTH_Z: number = -1.8093634648776054;
-const LENGTH: number = 20;
 
 export default class Collision {
 
@@ -20,8 +17,6 @@ export default class Collision {
         corners.push(new Vector3(-ABSOLUTE_CAR_LENGTH_X, ABSOLUTE_CAR_LENGTH_Y, ABSOLUTE_CAR_LENGTH_Z));
         corners.push(new Vector3(-ABSOLUTE_CAR_LENGTH_X, ABSOLUTE_CAR_LENGTH_Y, -ABSOLUTE_CAR_LENGTH_Z));
         corners.push(new Vector3(ABSOLUTE_CAR_LENGTH_X, ABSOLUTE_CAR_LENGTH_Y, -ABSOLUTE_CAR_LENGTH_Z));
-        const rotationMatrix: Matrix4 = new Matrix4();
-        rotationMatrix.extractRotation(car.mesh.matrix);
 
         for (const corner of corners) {
             const globalVertex: Vector3 = corner.applyMatrix4(car.mesh.matrix);
@@ -35,35 +30,16 @@ export default class Collision {
 
         return false;
     }
-    /*
-        MIN BOX: -1.8093634648776054, -0.007726105637907718, -0.7128778274977209
-        MAX BOX: 1.5854470477789642, 0.9663772207415103, 0.7128778274977209
-        CENTER BOX: -0.11195820854932059, 0.4793255575518013, 0
-        var originPoint = MovingCube.position.clone();
 
-        corners.push(new Vector3(car.boundingBox.min.x, car.boundingBox.min.y, car.boundingBox.min.z));
-
-        clearText();
-
-        const rotationMatrix: Matrix4 = new Matrix4();
-        const carDirection: Vector3 = new Vector3(0, 0, -1);
-
-        rotationMatrix.extractRotation(this._mesh.matrix);
-        carDirection.applyMatrix4(rotationMatrix);
-
-        for (var vertexIndex = 0; vertexIndex < MovingCube.geometry.vertices.length; vertexIndex++)
-        {
-            var localVertex = MovingCube.geometry.vertices[vertexIndex].clone();
-            var globalVertex = localVertex.applyMatrix4( MovingCube.matrix );
-            var directionVector = globalVertex.sub( MovingCube.position );
-
-            var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
-            var collisionResults = ray.intersectObjects( collidableMeshList );
-            if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
-                appendText(" Hit ");
+    public static detectOutOfBoundsAI(ai: Array<Car>, track: Mesh[]): boolean {
+        for (const car of ai) {
+            return Collision.detectOutOfBounds(car, track);
         }
+    }
 
-    */
+    public static collideOutOfBounds(car: Car, trackSegment: Mesh): void {
+
+    }
 
     public static collide(carA: Car, carB: Car): Array<Vector3> {
         const resultSpeeds: Array<Vector3> = [];
